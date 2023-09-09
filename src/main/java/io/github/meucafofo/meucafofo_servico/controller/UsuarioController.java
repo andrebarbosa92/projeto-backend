6cc92579d3eb4c332223a305bccb5986f03d1e01
Moisés Costa Caldas <sesiom.br@gmail.com>
2023-09-09 02:14:18 -0300

    📦 - Implementado fluxo de cadastro

--- src/main/java/io/github/meucafofo/meucafofo_servico/controller/UsuarioController.java
+++ src/main/java/io/github/meucafofo/meucafofo_servico/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 
 	@PostMapping
-	public ResponseEntity<Usuario> cadastrarUsuario(final CadastrarUsuarioDTO dto) {
+	public ResponseEntity<Usuario> cadastrarUsuario(final @RequestBody CadastrarUsuarioDTO dto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(dto));
 	}
 
